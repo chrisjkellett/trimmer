621f5a221786b9e1e0b324aeb005f592f383f2f9
chrisjkellett <cjkellett@hotmail.co.uk>
2019-05-22 18:20:43 +0200

    refactored tests and added readme

--- test/app.test.js
+++ test/app.test.js
@@ -1,52 +1,32 @@
 const Trimmer = require('../src/app');
-
 const trimmer = new Trimmer();
-const punctuation = '?>!!"';
-
-describe('trimmer.shouldCheck()', () => {
-  it('returns false when no non-alphanumeric character is present', () => {
-    expect(trimmer.shouldCheck()).toBe(false);
-    expect(trimmer.shouldCheck('stringWithoutPunctuation')).toBe(false);
-  })
-
-  it('returns true if any non-alphanumeric character is present', () => {
-    expect(trimmer.shouldCheck('hello!!!')).toBe(true);
-    expect(trimmer.shouldCheck(punctuation + 'hello')).toBe(true);
-    expect(trimmer.shouldCheck('hello' + punctuation)).toBe(true);
-  })
-})
 
 describe('trimmer.leading()', () => {
   it('returns null when no leading punctuation', () => {
-    const result = trimmer.leading('hello');
-    expect(result).toBeNull();
+    expect(trimmer.leading('hello')).toBeNull();
   });
 
   it('else returns leading punctuation', () => {
-    const result = trimmer.leading(punctuation + 'hello');
-    expect(result).toBe(punctuation);
+    expect(trimmer.leading('!!hello??')).toBe('!!');
   })
 })
 
 describe('trimmer.trailing()', () => {
   it('returns null when no trailing punctuation', () => {
-    const result = trimmer.trailing('hello');
-    expect(result).toBeNull();
+    expect(trimmer.trailing('hello')).toBeNull();
   });
 
   it('else returns leading punctuation', () => {
-    const result = trimmer.trailing('hello' + punctuation);
-    expect(result).toBe(punctuation);
+    expect(trimmer.trailing('!!hello??')).toBe('??');
   })
 })
 
 describe('trimmer.strip()', () => {
   it('returns string with no punctuation at word boundaries', () => {
-    expect(trimmer.strip(punctuation + 'hello')).toBe('hello');
-    expect(trimmer.strip('hello' + punctuation)).toBe('hello');
+    expect(trimmer.strip('!!hello??')).toBe('hello');
   })
 
   it('preserves punctuation within a word', () => {
-    expect(trimmer.strip('hello' + punctuation + 'world')).toBe('hello' + punctuation + 'world');
+    expect(trimmer.strip('!!hel-lo??')).toBe('hel-lo');
   })
 })
